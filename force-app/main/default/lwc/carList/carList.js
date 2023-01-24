ac3e2c7f4947c9d7d6d8762b6173c5e37d6f2546
Mariano <marianonunezma@gmail.com>
2023-01-24 16:05:45 -0300

    Adding a Filter

--- force-app/main/default/lwc/carList/carList.js
+++ force-app/main/default/lwc/carList/carList.js
@@ -1,6 +1,16 @@
-import { LightningElement, wire } from 'lwc';
-import getAllCars from '@salesforce/apex/CarServices.getAllCars';
+import { LightningElement, wire } from "lwc";
+import getAllCars from "@salesforce/apex/CarServices.getAllCars";
 
 export default class CarList extends LightningElement {
-    @wire(getAllCars) cars;
+  @wire(getAllCars) cars;
+
+  showConsole(event) {
+    const arrayDB = this.cars.data;
+
+    arrayDB.forEach(elem => {
+        if(elem.Name === event.target.value){
+            console.log(elem);
+        }
+    })
+  }
 }
